a5bb6c6599fb292214810ac1bdde79c521a07a8e
edinhox10 <edson.bol.santos@hotmail.com>
2021-06-11 20:19:07 -0300

    criando comida da cobra

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = "right";  
+let direction = "right"; 
+let food = {
+     x: Math.floor(math.random() * 15 + 1) * box,
+     y: Math.floor(math.random() * 15 + 1) * box
+
+
+
+
+} 
 
 function criarBG(){
     context.fillStyle = " lightgreen";
@@ -19,6 +27,11 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
 
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -40,6 +53,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
